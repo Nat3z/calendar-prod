7c12549d04718b74708dc39f2ca61fc5131365ca
Nat3z <djanice0408@yahoo.com>
2023-01-11 18:05:23 -0800

    switch to vercel serverless functions

--- server/api/schedule.ts
+++ server/api/schedule.ts
@@ -1,11 +1,9 @@
-import ical, { VEvent } from 'node-ical'
-import axios from 'axios'
-import express from 'express'
-
+import { VercelRequest, VercelResponse } from '@vercel/node';
 const matchRegex = /(.*?) (\d{1,2}:\d{2}(?: - |-|- )\d{1,2}:\d{2})(?:.*?)/gm;
-const app = express()
+import axios from 'axios';
+import * as ical from 'node-ical';
 
-app.get('/', async (req, res) => {
+export default async (req: VercelRequest, res: VercelResponse) => {
   let ics = (await axios.get("https://www.salesian.com/data/calendar/icalcache/calendar_369.ics")).data
   // parse the ics file with node-ical
   let events = await ical.async.parseICS(ics)
@@ -21,31 +19,27 @@ app.get('/', async (req, res) => {
       return event.summary.match(matchRegex) != null
     }
   })  
-  if (!event) return res.send({ events: null, code: 500, message: "Did not find VEevent for today." })
-  console.log(event.summary)
-
-  let times = new Map<string, number[]>()
+  if (!event) return res.json({ title: null, events: null, code: 500, message: "Did not find VEevent for today." })
+  let title = event.summary
+  let times = new Map<string, { start: number, end: number }>()
 
   let matchedTime: RegExpExecArray | null
   while ((matchedTime = matchRegex.exec(event.description)) !== null) {
     let time = matchedTime[2].replace(" - ", "-").replace(" ", "-").replace("- ", "-")
     let period = matchedTime[1].replace("-", "").replace(":", "").trim()
     
-    if (!time || !period) return res.send({ events: null, code: 500, message: "Did not find event time period in event description." })
+    if (!time || !period) return res.json({ title: null, events: null, code: 500, message: "Did not find event time period in event description." })
 
     let [start, end] = time.split("-").map(time => new Date(today.getFullYear(), today.getMonth(), today.getDate(), ...time.split(":").map(Number)))
     // get the period of the event
-    if (!start || !end) return res.send({ events: null, code: 500, message: "Did not find start and end times." })
+    if (!start || !end) return res.json({ title: null, events: null, code: 500, message: "Did not find start and end times." })
     
     // if the time is before 8am, add 12 hours to it
     if (start.getHours() < 8) start.setHours(start.getHours() + 12)
     if (end.getHours() < 8) end.setHours(end.getHours() + 12)
 
-    times.set(period, [ start.getTime() / 1000, end.getTime() / 1000 ])
+    times.set(period, { start: start.getTime() / 1000, end: end.getTime() / 1000 })
   }
 
-  return res.send({ events: Object.fromEntries(times), code: 200 })
-})
-app.listen(3001 || process.env.PORT, () => {
-  console.log('Server started on port 3001')
-})
+  return res.json({ title, events: Object.fromEntries(times), code: 200 })
+}
