eaceca601dbcb6eec9abec9ef27b02fc97ba9574
Nat3z <66748576+Nat3z@users.noreply.github.com>
2023-08-22 19:11:54 -0700

    pwa test

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -2,10 +2,34 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), AstroPWA({ 
+    mode: 'production',
+    base: '/',
+    scope: '/',
+    includeAssets: ['favicon.svg'],
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'DynSchedule',
+      short_name: 'DynSchedule',
+      theme_color: '#050014',
+      icons: [
+        {
+          src: 'grad_logo-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'grad_logo-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    }
+  }) ],
 });
