c3bfe18d43ec6b9df8d89b5381abb44962c83104
Nat3z <66748576+Nat3z@users.noreply.github.com>
2023-08-24 21:44:13 -0700

    checkScheduleValidity

--- server/api/checkScheduleValidity.ts
+++ server/api/checkScheduleValidity.ts
@@ -8,7 +8,9 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   let events = await ical.async.parseICS(ics)
   let vEvents = Object.values(events).filter(event => event.type == "VEVENT")
   let today = new Date()
-  const matchRegex = /(.*?) (\d{1,2}:\d{2}(?: - |-|- )\d{1,2}:\d{2})(?:.*?)/gm;
+
+  const matchRegex = /(.*?) (\d{1,2}:\d{2}(?:(?:\s+)?-(?:\s)?)\d{1,2}:\d{2})(?:.*?)/gm;
+  
   /* @ts-ignore */
   let event: ical.VEvent | undefined = vEvents.find(event => {
     if (event.type !== "VEVENT") return false
@@ -20,40 +22,49 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   if (!event) return res.send("No custom schedule found for today.")
   function sendErrorAPI(events: string[] = []) {
     // send an embed to the error channel through discord using the webhook and also @everyone
-    axios.post(process.env.DISCORD_WEBHOOK!, {
-      embeds: [
-        {
-          title: "Schedule Error",
-          description: `The schedule for ${today.toLocaleDateString()} is invalid. Please check the schedule and fix it.\n**Missing Events:** \n- ${events.join("\n- ")}`,
-          color: 0xff0000,
-          timestamp: new Date().toISOString(),
-          footer: {
-            text: "DynSchedule Alert"
-          },
-        }
-      ],
-      content: ""
+    var options = {
+      method: 'POST',
+      url: 'https://maker.ifttt.com/trigger/mangadex_update/with/key/' + process.env.ifttt,
+      headers: {
+        Accept: '*/*',
+        'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+        'Content-Type': 'application/json'
+      },
+      data: {
+        value1: '‼️ Schedule Issue Detected.',
+        value2: 'plz go fix ty',
+        value3: 'https://schedule.nat3z.com/'
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
     });
   }
   let title = event.summary
   let eventsToCheck: string[] = []
   let matchedTime: RegExpExecArray | null
   event.description = event.description.replace("8:20am - Welcome Bell", "")
-  while ((matchedTime = plainTextRegex.exec(event.description)) !== null) {
-    eventsToCheck.push(matchedTime[0].replace(/ \d+$/g, "").trim())
-  }
   // get the current url of this api
   let fromAPI = await axios.get(process.env.VERCEL_URL!.startsWith("localhost") ? `http://${process.env.VERCEL_URL}/api/schedule` : `https://${process.env.VERCEL_URL}/api/schedule`)
   let fromAPIEvents = fromAPI.data.events
   let fromAPIEventNames = Object.keys(fromAPIEvents)
-  let eventsNotFound: string[] = []
-  for (const event of eventsToCheck) {
-    if (!fromAPIEventNames.includes(event)) {
-      eventsNotFound.push(event)
+  let eventsErrored: string[] = []
+  for (const event of fromAPIEventNames) {
+    const schedule = fromAPIEvents[event]
+    if (!schedule.start || !schedule.end) {
+      eventsErrored.push(event)
+      continue
+    }
+    else if (schedule.start === schedule.end) {
+      eventsErrored.push(event)
+      continue
     }
   }
-  if (eventsNotFound.length > 0) {
-    sendErrorAPI(eventsNotFound)
+  if (eventsErrored.length > 0) {
+    sendErrorAPI(eventsErrored)
     return res.send("Schedule is invalid.")
   }
   return res.send("Schedule is valid.")
